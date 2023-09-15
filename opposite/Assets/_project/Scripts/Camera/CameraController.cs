99f3c7026792a2ee4163057993c909a3298796ea
Mohamed Anes Mostefaoui <m.anas@outlook.com>
2023-09-15 13:03:06 +0300

    Implement camera movement. Implement parallax effect. Add multiple background layers.

--- opposite/Assets/_project/Scripts/Camera/CameraController.cs
+++ opposite/Assets/_project/Scripts/Camera/CameraController.cs
@@ -1,6 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace OppositeGame
 {
-    public class CameraController : MonoBehaviour {}
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform player;
+        [SerializeField] private float speed = 2f;
+
+        private void Start()
+        {
+            if (player == null)
+            {
+                player = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+
+            var playerPosition = player.position;
+            transform.position = new Vector3(playerPosition.x, playerPosition.y, transform.position.z);
+        }
+
+        private void LateUpdate()
+        {
+            // we want the camera to move last.
+            transform.position += Vector3.up * (speed * Time.deltaTime);
+        }
+    }
 }
--- opposite/Assets/_project/Scripts/Environment/ParallaxController.cs
+++ opposite/Assets/_project/Scripts/Environment/ParallaxController.cs
@@ -1,6 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace OppositeGame
 {
-    public class ParallaxController : MonoBehaviour {}
+    public class ParallaxController : MonoBehaviour
+    {
+        [SerializeField] private Transform[] backgrounds;
+        [SerializeField] private float parallaxSmoothing = 10f;
+        [SerializeField] private float multiplier = 15f;
+        
+        private GameObject _camera;
+        private Vector3 _previousCameraPosition;
+        
+        private void Awake()
+        {
+            if (_camera == null)
+            {
+                _camera = Camera.main.gameObject;
+            }
+        }
+        private void Start()
+        {
+            _previousCameraPosition = _camera.transform.position;
+        }
+
+        private void Update()
+        {
+            var cameraPosition = _camera.transform.position;
+            for (var i = 0; i < backgrounds.Length; i++)
+            {
+                // The array will move from the back to the front
+                // The first background will be the slowest to move, the second background will move faster, and so on
+               var background = backgrounds[i];
+               var movementDelta = (_previousCameraPosition.y - cameraPosition.y) * (i * multiplier);
+               var backgroundPosition = background.position;
+               var verticalPosition = backgroundPosition.y + movementDelta;
+               // set the new position of the background based on the camera movement, it's position in the array, the multiplier, and the smoothing factor
+               var destination = new Vector3(backgroundPosition.x, verticalPosition, backgroundPosition.z);
+               // interpolate the background's position to the new destination with time to avoid a jarring movement
+               backgroundPosition = Vector3.Lerp(backgroundPosition, destination, parallaxSmoothing * Time.deltaTime);
+               background.position = backgroundPosition;
+            }
+            // cache the camera's position for the next frame
+            _previousCameraPosition = cameraPosition;
+        }
+    }
 }
