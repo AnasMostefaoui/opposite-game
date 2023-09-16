57a7e35bd33ac4b94249ca16a965e7d5553405b3
Mohamed Anes Mostefaoui <m.anas@outlook.com>
2023-09-16 15:05:02 +0300

    Implement view observer to allow actions when we enter view or we leave it.

--- opposite/Assets/_project/Scripts/mechanics/Movement/ViewPortObserver.cs
+++ opposite/Assets/_project/Scripts/mechanics/Movement/ViewPortObserver.cs
@@ -1,7 +1,57 @@
-﻿namespace OppositeGame._project.Scripts.mechanics
+﻿using System;
+using OppositeGame._project.Scripts.Utilities;
+using UnityEngine;
+
+namespace OppositeGame._project.Scripts.mechanics.Movement
 {
-    public class ViewPortObserver
+    public class ViewPortObserver: MonoBehaviour
     {
+        public Action OnEnteredViewport;
+        public Action OnLeftViewport;
         
+        
+        private bool _isInViewport;
+        private bool _hasEnteredViewport;
+        private Camera _camera;
+        private const float SafeZone = 1f;
+
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+        
+        private void FixedUpdate()
+        {
+            if (_isInViewport)
+            {
+                
+            }
+            var transformPosition = transform.position + new Vector3(0, SafeZone, 0);
+            _isInViewport = _camera.IsPointInViewport(transformPosition);
+
+            if (_hasEnteredViewport == false && _isInViewport)
+            {
+                _hasEnteredViewport = true;
+                ObjectEnteredViewport();
+            }
+            
+           
+            if (_isInViewport == false && _hasEnteredViewport == true)
+            {
+                ObjectLeftViewport();
+            }
+        }
+        
+        // This can be an event delegate to allow other scripts to hook function when an object enter screen
+        private void ObjectEnteredViewport()
+        {
+            OnEnteredViewport?.Invoke();
+        }
+
+        private void ObjectLeftViewport()
+        {
+            OnLeftViewport?.Invoke();
+        }
     }
 }
