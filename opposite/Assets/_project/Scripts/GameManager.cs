2819981f4632a1d886c04a69c3c1a1fec9198387
Mohamed Anes Mostefaoui <m.anas@outlook.com>
2023-09-16 18:45:55 +0300

    Add draft of game manager

---
+++ opposite/Assets/_project/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿namespace OppositeGame._project.Scripts
+{
+    public class GameManager
+    {
+        private static GameManager _instance;
+        public static GameManager Instance => _instance ??= new GameManager();
+        
+        public int Score { get; private set; }
+        public int isGameOver { get; private set; }
+        public int isPaused { get; private set; }
+        public float timeScale { get; private set; }
+        
+        public event System.Action<int> OnScoreChanged;
+        
+        public void AddScore(int score)
+        {
+            Score += score;
+            OnScoreChanged?.Invoke(Score);
+        }
+        
+    }
+}
