3cd931a083e7a011421b2d0acbb515221c07e55b
Mohamed Anes Mostefaoui <m.anas@outlook.com>
2023-09-17 18:41:06 +0300

    Disable shooting off screen

--- opposite/Assets/_project/Scripts/mechanics/Weapons/EnemyWeapon.cs
+++ opposite/Assets/_project/Scripts/mechanics/Weapons/EnemyWeapon.cs
@@ -1,14 +1,24 @@
-﻿using UnityEngine;
+﻿using OppositeGame._project.Scripts.CameraScripts;
+using OppositeGame._project.Scripts.Utilities;
+using UnityEngine;
 
 namespace OppositeGame._project.Scripts.mechanics.weapons
 {
     public class EnemyWeapon : Weapon
     {
+        [SerializeField] private Camera camera;
         private float _nextFireTime;
+        private bool IsInViewPort => camera.IsPointInViewport(transform.position);
+        
+        private void Awake()
+        {
+            weaponStrategy.Initialize();
+            camera ??= Camera.main;
+        }
         
         private void Update()
         {
-            if (!(Time.time >= _nextFireTime)) return;
+            if (!IsInViewPort || !(Time.time >= _nextFireTime)) return;
             
             weaponStrategy.Fire(startTransform, layer);
             _nextFireTime = Time.time + weaponStrategy.fireRate;
