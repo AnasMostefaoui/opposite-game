38c5a27f72642f81c634b6ea6f9b85a89672a8bb
Mohamed Anes Mostefaoui <m.anas@outlook.com>
2023-09-18 18:34:02 +0300

    Create death zone to test screen flows.

---
+++ opposite/Assets/_project/Scripts/Environment/DeathZone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace OppositeGame._project.Scripts.Environment
+{
+    public class DeathZone : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                GameManager.Instance.IsGameOver = true;
+            }
+        }
+    }
+}
