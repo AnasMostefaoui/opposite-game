58e31f7788fc2ce69a143fc8e06f62723819b598
Anas Mostefaoui <m.anas@outlook.com>
2023-09-22 12:21:51 +0300

    Add laser trap

---
+++ opposite/Assets/LaserRenderer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OppositeGame
+{
+    public class LaserRenderer : MonoBehaviour
+    {
+        private LineRenderer r;
+        public SpriteMaskInteraction maskInteraction;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            void Update()
+            {
+                // r.maskInteraction = maskInteraction;
+            }
+        }
+    }
+}
